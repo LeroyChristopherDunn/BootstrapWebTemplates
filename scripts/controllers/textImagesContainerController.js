b0c0ebb2a776b1274f8e4afcd152e3fb195d5944
Leroy Dunn <Leroy.Dunn@investec.co.za>
2017-11-24 17:36:23 +0200

    feat: half complete text images container

---
+++ scripts/controllers/textImagesContainerController.js
@@ -0,0 +1,11 @@
+app.controller('textImagesContainerController', function($scope) {
+
+    $scope.images = [
+        {id: 1, url: "http://placehold.it/400x300"},
+        {id: 2, url: "http://placehold.it/400x300"},
+        {id: 3, url: "http://placehold.it/400x300"},
+        {id: 4, url: "http://placehold.it/400x300"},
+        {id: 5, url: "http://placehold.it/400x300"},
+        {id: 6, url: "http://placehold.it/400x300"},
+    ]
+});
