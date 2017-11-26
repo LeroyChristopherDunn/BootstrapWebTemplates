8a18d0f5eaecd3cb1de96691b0b52536d01b160c
leroy <leroy.c.dunn@gmail.com>
2017-11-26 14:08:48 +0200

    feat: responsive image loading

--- scripts/controllers/textImagesContainerController.js
+++ scripts/controllers/textImagesContainerController.js
@@ -1,6 +1,13 @@
-app.controller('textImagesContainerController', function($scope) {
+app.controller('textImagesContainerController', function($scope, $window) {
 
-    $scope.images = [
+    $scope.images = getImages(window.innerWidth);
+});
+
+function getImages(windowWidth){
+
+    const smallWindowMaxWidth = 767;
+
+    const images = [
         {id: 1, url: "http://placehold.it/400x300"},
         {id: 2, url: "http://placehold.it/400x300"},
         {id: 3, url: "http://placehold.it/400x300"},
@@ -8,4 +15,8 @@ app.controller('textImagesContainerController', function($scope) {
         {id: 5, url: "http://placehold.it/400x300"},
         {id: 6, url: "http://placehold.it/400x300"},
     ]
-});
+
+    if(windowWidth < smallWindowMaxWidth)
+        return images.slice(0, 2);
+    return images;
+}
