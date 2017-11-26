b57f3d104ae6e8fe9ef20724d7ce4b9288710f1b
leroy <leroy.c.dunn@gmail.com>
2017-11-26 16:33:25 +0200

    feat: minor decoupling of data from view

--- scripts/controllers/textImagesContainerController.js
+++ scripts/controllers/textImagesContainerController.js
@@ -1,7 +1,20 @@
 app.controller('textImagesContainerController', function($scope, $window) {
 
+    $scope.heading = getHeading();
+    $scope.paragraphs = getParagraphs();
     $scope.images = getImages(window.innerWidth);
 });
+
+function getHeading(){
+    return "Heading";
+}
+
+function getParagraphs(){
+    return [
+        {id: 1, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Placerat duis ultricies lacus sed turpis."},
+        {id: 2, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Placerat duis ultricies lacus sed turpis."}
+    ]
+}
 
 function getImages(windowWidth){
 
